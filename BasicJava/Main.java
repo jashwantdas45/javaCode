ba2503753f7ddfed967203ef8b6c69ba6727e814
Jashwant Das <jashwantswami367@gmail.com>
2026-03-30 16:30:48 +0530

    first commit

---
+++ BasicJava/Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Java");
+
+        int num = 1234;
+        int reverse = 0;
+
+        for (; num != 0; num = num / 10) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+        }
+
+        System.out.println("Number is " + reverse);
+    }
+}
